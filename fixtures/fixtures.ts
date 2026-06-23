951d203fac28902644a1cc5a63fc0e213a1c3e6b
Martin Gabor <gabormartin94@gmail.com>
2026-06-23 15:41:24 +0200

    Added productsPage as fixture

--- fixtures/fixtures.ts
+++ fixtures/fixtures.ts
@@ -1,8 +1,10 @@
 import { test as base } from "@playwright/test";
 import { LoginPage } from "@pages/LoginPage";
+import { ProductsPage } from "@pages/ProductsPage";
 
 type MyFixtures = {
   loginPage: LoginPage;
+  productsPage: ProductsPage;
 };
 
 export const test = base.extend<MyFixtures>({
@@ -10,5 +12,11 @@ export const test = base.extend<MyFixtures>({
     const loginPage = new LoginPage(page);
     await loginPage.open();
     await use(loginPage);
+  },
+
+  productsPage: async ({ page, loginPage }, use) => {
+    await loginPage.login("standard_user", "secret_sauce");
+    const productsPage = new ProductsPage(page);
+    await use(productsPage);
   },
 });
