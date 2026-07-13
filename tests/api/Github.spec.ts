ab955f6b4bdaf3b1b4902795df99e27232c0479d
Martin Gabor <gabormartin94@gmail.com>
2026-07-13 14:47:07 +0200

    Added star testcase, refactore status code checks

--- tests/api/Github.spec.ts
+++ tests/api/Github.spec.ts
@@ -1,15 +1,18 @@
 import { test, expect } from "fixtures/api-fixtures";
 import {
+  checkRepositoryStarred,
   createIssue,
-  retrieveIssues,
   getRepository,
+  retrieveIssues,
+  starRepository,
+  unstarRepository,
   waitForItemInList,
 } from "utils/api-helper";
 import { bug, USER } from "testdata/constants";
 
 test("Should retrieve repository by name", async ({ request, repoName }) => {
   const response = await getRepository(request, USER, repoName);
-  expect(response.ok()).toBeTruthy();
+  expect(response.status()).toBe(200);
   const responseBody = await response.json();
   expect(responseBody.full_name).toBe(`${USER}/${repoName}`);
 });
@@ -19,10 +22,27 @@ test("Should create a bug and verifies it's present in bugs list", async ({
   repoName,
 }) => {
   const response = await createIssue(request, USER, repoName, bug);
-  expect(response.ok()).toBeTruthy();
+  expect(response.status()).toBe(201);
 
   await waitForItemInList(async () => {
     const issues = await retrieveIssues(request, USER, repoName);
     return issues.json();
   }, bug);
+});
+
+test("Should star and unstar repository", async ({ request, repoName }) => {
+  const initialCheck = await checkRepositoryStarred(request, USER, repoName);
+  expect(initialCheck.status()).toBe(404);
+
+  const starResponse = await starRepository(request, USER, repoName);
+  expect(starResponse.status()).toBe(204);
+
+  const starredCheck = await checkRepositoryStarred(request, USER, repoName);
+  expect(starredCheck.status()).toBe(204);
+
+  const unstarResponse = await unstarRepository(request, USER, repoName);
+  expect(unstarResponse.status()).toBe(204);
+
+  const finalCheck = await checkRepositoryStarred(request, USER, repoName);
+  expect(finalCheck.status()).toBe(404);
 });
