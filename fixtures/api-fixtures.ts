bd8a88b2b8dc4933599b899051cb126d5334aafb
Martin Gabor <gabormartin94@gmail.com>
2026-07-13 14:11:27 +0200

    Added api fixtures

---
+++ fixtures/api-fixtures.ts
@@ -0,0 +1,23 @@
+// utils/fixtures/api-fixtures.ts
+import { test as base, expect } from "@playwright/test";
+import { createRepository, deleteRepository } from "../utils/api-helper";
+import { USER } from "utils/constants";
+
+type ApiFixtures = {
+  repoName: string;
+};
+
+export const test = base.extend<ApiFixtures>({
+  repoName: async ({ request }, use, testInfo) => {
+    const repoName = `ApiTesting-${testInfo.testId}`;
+    const createResponse = await createRepository(request, repoName);
+    expect(createResponse.ok()).toBeTruthy();
+
+    await use(repoName);
+
+    const deleteResponse = await deleteRepository(request, USER, repoName);
+    expect(deleteResponse.ok()).toBeTruthy();
+  },
+});
+
+export { expect };
