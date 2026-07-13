e1a7772311c3952d9f7feb064dc8452638c27042
Martin Gabor <gabormartin94@gmail.com>
2026-07-13 14:13:04 +0200

    Refactor with fixture

--- tests/api/Github.spec.ts
+++ tests/api/Github.spec.ts
@@ -1,26 +1,35 @@
-import test, { expect, request } from "@playwright/test";
+import { test, expect } from "fixtures/api-fixtures";
 import {
+  createBug,
   createRepository,
   deleteRepository,
+  getBugs,
   getRepository,
+  waitForItemInList,
 } from "utils/api-helper";
+import { USER } from "utils/constants";
 
-const REPO = "ApiTesting";
-const USER = "GaborMartin";
+const bug = {
+  title: "[Bug] report 1",
+  body: "Bug description",
+};
 
-test.beforeAll(async ({ request }) => {
-  const response = await createRepository(request, REPO);
-  expect(response.ok()).toBeTruthy();
-});
-
-test("Should get repository by name", async ({ request }) => {
-  const response = await getRepository(request, USER, REPO);
+test("Should retrieve repository by name", async ({ request, repoName }) => {
+  const response = await getRepository(request, USER, repoName);
   expect(response.ok()).toBeTruthy();
   const responseBody = await response.json();
-  expect(responseBody.full_name).toBe(`${USER}/${REPO}`);
+  expect(responseBody.full_name).toBe(`${USER}/${repoName}`);
 });
 
-test.afterAll(async ({ request }) => {
-  const response = await deleteRepository(request, USER, REPO);
+test("Should create a bug and verifies it's present in bugs list", async ({
+  request,
+  repoName,
+}) => {
+  const response = await createBug(request, USER, repoName, bug);
   expect(response.ok()).toBeTruthy();
+
+  await waitForItemInList(async () => {
+    const bugs = await getBugs(request, USER, repoName);
+    return bugs.json();
+  }, bug);
 });
