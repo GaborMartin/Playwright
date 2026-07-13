ed3cc30a7bfebd607f7677f789a431c83f7381d4
Martin Gabor <gabormartin94@gmail.com>
2026-07-13 20:21:46 +0200

    Added repository functionality

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -5,12 +5,21 @@ export class HomePage {
   readonly logoLink: Locator;
   readonly dashboardLink: Locator;
   readonly header: Locator;
+  readonly userNavigationBtn: Locator;
+  readonly repositoriesLink: Locator;
 
   constructor(private page: Page) {
     this.menuBtn = page.getByRole("button", { name: "Open menu" });
     this.logoLink = page.getByRole("link", { name: "Homepage (g then d)" });
     this.dashboardLink = page.getByRole("link", { name: "Dashboard" });
     this.header = page.getByRole("heading", { name: "Home" });
+    this.userNavigationBtn = page.getByRole("button", {
+      name: "Open user navigation menu",
+    });
+    this.repositoriesLink = page.getByRole("link", {
+      name: "Repositories",
+      exact: true,
+    });
   }
 
   async expectPageVisible() {
@@ -18,5 +27,10 @@ export class HomePage {
     await expect(this.logoLink).toBeVisible();
     await expect(this.dashboardLink).toBeVisible();
     await expect(this.header).toBeVisible();
+  }
+
+  async navigateToRepositories() {
+    await this.userNavigationBtn.click();
+    await this.repositoriesLink.click();
   }
 }
