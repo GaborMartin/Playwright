eebb31a95647a57a802ffe1080342f7fbd4c660b
Martin Gabor <gabormartin94@gmail.com>
2026-07-13 20:21:07 +0200

    Implemented github specific settings

--- fixtures/api-fixtures.ts
+++ fixtures/api-fixtures.ts
@@ -1,20 +1,42 @@
-import { test as base, expect } from "@playwright/test";
+import {
+  APIRequestContext,
+  test as base,
+  expect,
+  request as playwrightRequest,
+} from "@playwright/test";
 import { createRepository, deleteRepository } from "../utils/api-helper";
 import { USER } from "testdata/constants";
 
 type ApiFixtures = {
-  repository: string;
+  repositoryName: string;
+  githubRequest: APIRequestContext;
 };
 
 export const test = base.extend<ApiFixtures>({
-  repository: async ({ request }, use, testInfo) => {
+  githubRequest: async ({}, use) => {
+    const context = await playwrightRequest.newContext({
+      baseURL: "https://api.github.com",
+      extraHTTPHeaders: {
+        Authorization: `Bearer ${process.env.API_TOKEN}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+    await use(context);
+    await context.dispose(); // clean up the context after the test
+  },
+
+  repositoryName: async ({ githubRequest }, use, testInfo) => {
     const repoName = `ApiTesting-${testInfo.testId}`;
-    const createResponse = await createRepository(request, repoName);
+    const createResponse = await createRepository(githubRequest, repoName);
     expect(createResponse.status()).toBe(201);
 
     await use(repoName);
 
-    const deleteResponse = await deleteRepository(request, USER, repoName);
+    const deleteResponse = await deleteRepository(
+      githubRequest,
+      USER,
+      repoName,
+    );
     expect(deleteResponse.status()).toBe(204);
   },
 });
