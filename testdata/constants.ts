47363af4a16aa61902a1434f8dad76c77dcd9cd2
Martin Gabor <gabormartin94@gmail.com>
2026-07-13 14:17:44 +0200

    Moved constant to testdata

---
+++ testdata/constants.ts
@@ -0,0 +1,9 @@
+export const USER = "GaborMartin";
+export const bug = {
+  title: "[Bug] report 1",
+  body: "Bug description",
+};
+export const featureRequest = {
+  title: "[Feature] request 1",
+  body: "Feature description",
+};
