a3b854ec2ff8e0c36ee086ac4a12bd6dc2f3f685
Martin Gabor <gabormartin94@gmail.com>
2026-07-14 09:02:20 +0200

    Removed login

--- tests/ui/desktop/GitHubUI.spec.ts
+++ tests/ui/desktop/GitHubUI.spec.ts
@@ -1,13 +1,7 @@
 import { test, expect } from "fixtures/index";
 
 test.describe("GitHub UI tests", () => {
-  test("Should have correct title", async ({ loginPage }) => {
-    await loginPage.expectTitleVisible();
-  });
-
-  test("Homepage should be correctly visible after login", async ({
-    homePage,
-  }) => {
+  test("Homepage should be correctly visible", async ({ homePage }) => {
     await homePage.expectPageVisible();
   });
 
