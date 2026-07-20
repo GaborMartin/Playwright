e2da8fddb51f9a2177f74d6bc29126e4b06888df
Martin Gabor <gabormartin94@gmail.com>
2026-07-20 08:09:30 +0200

    Added comment

--- tests/ui/desktop/GitHubUI.spec.ts
+++ tests/ui/desktop/GitHubUI.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "fixtures/index";
 
+//Test comment
 test.describe("GitHub UI tests", () => {
   test("Homepage should be correctly visible", async ({ homePage }) => {
     await homePage.expectPageVisible();
